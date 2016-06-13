159303c0b2ef2ffe07e9a7d2a3c1bbd6089c60d6
毛巴马 <xudaojie1992@126.com>
2016-06-13 11:12:00 +0800

    add NestedScrollView nested RecyclerView

---
+++ app/src/main/java/com/example/xdj/androiddemo/recyclerviewsample/RecyclerAdapter.java
@@ -0,0 +1,10 @@
+package com.example.xdj.androiddemo.recyclerviewsample;
+
+import com.example.xdj.androiddemo.BaseActivity;
+
+/**
+ * Created by xdj on 16/6/13.
+ */
+
+public class RecyclerAdapter extends BaseActivity {
+}
---
+++ app/src/main/java/com/example/xdj/androiddemo/recyclerviewsample/ScrollRecyclerActivity.java
@@ -0,0 +1,8 @@
+package com.example.xdj.androiddemo.recyclerviewsample;
+
+/**
+ * Created by xdj on 16/6/13.
+ */
+
+public class ScrollRecyclerActivity {
+}
