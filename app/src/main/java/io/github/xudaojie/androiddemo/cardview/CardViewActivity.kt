ad7ab6815b21ad454210bfc640406b71a63f8f78
XuDaojie <xudaojieg@gmail.com>
2016-07-20 00:27:39 +0800

    增加kotlin module

---
+++ app/src/main/java/io/github/xudaojie/androiddemo/cardview/CardViewActivity.kt
@@ -0,0 +1,17 @@
+package io.github.xudaojie.androiddemo.cardview
+
+import android.os.Bundle
+
+import io.github.xudaojie.androiddemo.BaseActivity
+import io.github.xudaojie.androiddemo.R
+
+/**
+ * Created by xdj on 16/3/6.
+ * CardView 示例
+ */
+class CardViewActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.card_view_activity)
+    }
+}
