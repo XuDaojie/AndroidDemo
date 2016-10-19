dcd03df579e1532e18c786b4187c66c0bcfd73cf
XuDaojie <xudaojieg@gmail.com>
2016-10-20 00:31:40 +0800

    简易GridLayoutManager

--- recyclerview/src/main/java/io/github/xudaojie/recyclerview/MyGridLayoutManager.java
+++ recyclerview/src/main/java/io/github/xudaojie/recyclerview/MyGridLayoutManager.java
@@ -12,13 +12,19 @@ import java.util.List;
  * Created by xdj on 2016/10/15.
  */
 
-public class MyExpandGridLayoutManager extends RecyclerView.LayoutManager {
-    private static final String TAG = MyExpandGridLayoutManager.class.getSimpleName();
+public class MyGridLayoutManager extends RecyclerView.LayoutManager {
+    private static final String TAG = MyGridLayoutManager.class.getSimpleName();
 
     private int mTotalOffsetY;
     private int mValidOffset;
 
     private SparseArray<Rect> mItemFrames = new SparseArray<>();
+
+    public int mSpanCount;
+
+    public MyGridLayoutManager(int spanCount) {
+        mSpanCount = spanCount;
+    }
 
     @Override
     public RecyclerView.LayoutParams generateDefaultLayoutParams() {
@@ -26,14 +32,6 @@ public class MyExpandGridLayoutManager extends RecyclerView.LayoutManager {
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
     }
-
-//    @Override
-//    public RecyclerView.LayoutParams generateLayoutParams(Context c, AttributeSet attrs) {
-//        return super.generateLayoutParams(c, attrs);
-//        return new RecyclerView.LayoutParams(
-//                ViewGroup.LayoutParams.WRAP_CONTENT,
-//                ViewGroup.LayoutParams.WRAP_CONTENT);
-//    }
 
     @Override
     public RecyclerView.LayoutParams generateLayoutParams(ViewGroup.LayoutParams lp) {
@@ -66,25 +64,14 @@ public class MyExpandGridLayoutManager extends RecyclerView.LayoutManager {
             if (frame == null) {
                 frame = new Rect();
             }
-//            if (i % 6 == 0) {
-//                // Group
-//                // Grid未充满一行时
-//                if (gridPosition != 0) {
-//                    offsetY += childHeight;
-//                }
-//                frame.set(0, offsetY, groupWidth, offsetY + childHeight);
-//                offsetY += childHeight;
-//                gridPosition = 0;
-//            } else {
-                // Child
-                frame.set(gridPosition * childWidth, offsetY, gridPosition * childWidth + childWidth, offsetY + childHeight);
-                if (gridPosition == 3) {
-                    gridPosition = 0;
-                    offsetY += childHeight;
-                } else {
-                    ++gridPosition;
-                }
-//            }
+            // Child
+            frame.set(gridPosition * childWidth, offsetY, gridPosition * childWidth + childWidth, offsetY + childHeight);
+            if (gridPosition == mSpanCount - 1) {
+                gridPosition = 0;
+                offsetY += childHeight;
+            } else {
+                ++gridPosition;
+            }
 //            frame.set(0, offsetY, childWidth, offsetY + childHeight);
             //将竖直方向偏移量增大height
             mItemFrames.put(i, frame);
@@ -130,42 +117,6 @@ public class MyExpandGridLayoutManager extends RecyclerView.LayoutManager {
 
         // todo ?? 跳过preLayout，preLayout主要用于支持动画
         if (!state.isPreLayout()) {
-//            // 回收未显示的View，并显示新的View
-//            Rect displayRect = new Rect(0, mValidOffset, getHorizontalSpace(), mValidOffset + getVerticalSpace());
-//            Log.d(TAG, 0 + ", " + mValidOffset + ", " + getHorizontalSpace() + ", " + (mValidOffset + getVerticalSpace()));
-//            Rect childRect = new Rect();
-//            Log.d(TAG, "remove before --getChildCount " + getChildCount() + " " + "getItemCount" + getItemCount());
-//            for (int i = 0; i < getChildCount(); i++) {
-//                View child = getChildAt(i);
-//                childRect.left = getDecoratedLeft(child);
-//                childRect.top = getDecoratedTop(child);
-//                childRect.right = getDecoratedRight(child);
-//                childRect.bottom = getDecoratedBottom(child);
-//
-//                if (!Rect.intersects(displayRect, childRect)) {
-////                    removeAndRecycleView(child, recycler);
-////                    detachAndScrapView(child, recycler);
-//                }
-//            }
-//            Log.d(TAG, "remove after --getChildCount " + getChildCount() + " " + "getItemCount" + getItemCount());
-//
-//            for (int i = 0; i < getItemCount(); i++) {
-//                if (Rect.intersects(displayRect, mItemFrames.get(i))) {
-//                    //
-//                    View scrap = recycler.getViewForPosition(i);
-//                    measureChildWithMargins(scrap, 0, 0);
-//                    addView(scrap);
-//
-//                    Rect frame = mItemFrames.get(i);
-//                    layoutDecorated(
-//                            scrap,
-//                            frame.left,
-//                            frame.top - mValidOffset,
-//                            frame.right,
-//                            frame.bottom - mValidOffset);
-//                }
-//            }
-//            Log.d(TAG, "getChildCount " + getChildCount() + " " + "getItemCount" + getItemCount());
             fill(recycler, state);
         }
 
